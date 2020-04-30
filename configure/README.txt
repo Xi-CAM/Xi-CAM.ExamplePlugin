3564a1258816538fe51cc2d13e6c29baa2153c46
Ian Humphrey <ian.r.humphrey@gmail.com>
2020-04-29 18:13:45 -0700

    Add sample configuration for catalogs and sample catalog

---
+++ configure/README.txt
@@ -0,0 +1,5 @@
+This directory is used for example purposes.
+
+It sets up the user's catalog_search_path directory for databroker,
+copies a catalog configuration to that directory,
+and copies msgpack catalogs to that directory as well.
---
+++ configure/setup_catalog.py
@@ -0,0 +1,25 @@
+# This script:
+#   - sets up the catalog_search_path directory for databroker to find catalogs
+#   - copies a simple catalog.yml file to configure our catalog sources
+#   - copies msgpack catalogs next to the catalog.yml
+
+import shutil
+from pathlib import Path
+from databroker import catalog_search_path
+
+catalog_path = catalog_search_path()[0]
+print(f"Using the following catalog search path: {catalog_path}...")
+
+print(f"Creating the directory {catalog_path} (if it does not already exist)...")
+Path(catalog_path).mkdir(parents=True, exist_ok=True)
+
+print(f"Copying catalog.yml to {catalog_path}...")
+shutil.copy("catalog.yml", catalog_path)
+
+print(f"Copying msgpack files to {catalog_path}...")
+msgpack_file_gen = Path.cwd().glob("*.msgpack")
+for f in msgpack_file_gen:
+    print(f"\tcopying {f.name}...")
+    shutil.copy(f, catalog_path)
+
+print("Done.")
